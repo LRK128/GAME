8fdbcbd811e7c771093510314e38e9ffa25ce209
JMB1304 <jbarnesusa@gmail.com>
2015-12-07 22:18:51 -0600

    moved game files
    moved game to new repo and created new binaries

---
+++ ArenaFighter/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHandler : MonoBehaviour {
+	
+	    public void multiplayerButton() {
+		GameLevel.loadMultiplayerMenu();
+		}
+	
+		public void exitButton() {
+		GameLevel.loadMainMenu();
+		}
+
+	public void demoButton(){
+		GameLevel.loadDemoLevel ();
+	}
+
+}
---
+++ ArenaFighter/Assets/Scripts/GameLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLevel : MonoBehaviour {
+	
+		public static void loadMainMenu() {
+			Application.LoadLevel ("MainMenu");
+		}
+
+		public static void loadMultiplayerMenu() {
+			Application.LoadLevel ("MultiplayerMenu");
+		}
+	public static void loadDemoLevel() {
+		Application.LoadLevel ("Demogame");
+	}
+
+}
